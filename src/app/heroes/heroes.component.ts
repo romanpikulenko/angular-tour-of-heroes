53e42639b72553f0fcfa968ca9f4802edcec0c56
roman.pikulenko <roman.pikulenko@gmail.com>
2023-03-05 15:51:09 +0100

    Added HeroService and MessageService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +9,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
